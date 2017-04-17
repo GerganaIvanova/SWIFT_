f56d16e1b675bb8c4f7fe46a94aebc4f1d2d68e8
Gergorana <Gergorana@DESKTOP-00618V7>
2017-04-17 13:04:31 +0300

    firstExamples

--- Homework7/src/homework7/main.java
+++ Homework7/src/homework7/main.java
@@ -1,28 +1,23 @@
 package homework7;
 
 import java.util.Scanner;
-//import java.lang.Math;
 
 public class main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String [] firstName = new String[2];
+        String[] firstName = new String[2];
         firstName[0] = sc.nextLine();
         firstName[1] = sc.nextLine();
-        String [] lastName = new String [2];
+        String[] lastName = new String[2];
         lastName[0] = sc.nextLine();
         lastName[1] = sc.nextLine();
         double weekSalary = sc.nextInt();
         double workHoursPerDay = sc.nextInt();
-        //double daySalary = sc.nextInt();
-        //double hourSalary = sc.nextInt();
         Worker teacher = new Worker(firstName[0], lastName[0], weekSalary, workHoursPerDay);
         int facultyNumber = sc.nextInt();
         int lectureCount = sc.nextInt();
         int exerciseCount = sc.nextInt();
-        //int dayLectures = sc.nextInt();
-        //int dayExercises = sc.nextInt();
         Student student = new Student(firstName[1], lastName[1], facultyNumber, lectureCount, exerciseCount);
 
         teacher.Salary();
